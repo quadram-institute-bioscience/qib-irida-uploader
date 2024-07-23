092e7f4f8f701b2e726a740807430ed95cd340d0
thanhleviet <thanh.le-viet@quadram.ac.uk>
2024-07-23 16:01:15 +0000

    tidy up

--- config.py
+++
@@ -1,8 +0,0 @@
-#Configs for huey
-from huey import PriorityRedisHuey, SqliteHuey
-
-huey = SqliteHuey(filename='queue.db')
-#TODO: switch password value to ENV
-# huey = PriorityRedisHuey('covid', host='localhost',
-#                          password='pAE7x8pcUKk6QjPg')
-huey.immediate = True
